286b8b70c48c05b605c8d0bff32fc935a7676eb6
Andrés Contreras <aecontrerasl@gmail.com>
2014-04-12 21:45:39 -0300

    Cargas.JS

--- ALMA_PROJECT/www/js/cargas.js
+++ ALMA_PROJECT/www/js/cargas.js
@@ -19,12 +19,15 @@ function processData(allText) {
             var antena = new Object();
 
             for (var j=0; j<headers.length; j++) {
-            	
-            	antena[headers[j]]=+data[j];
+                
+                antena[headers[j]]=+data[j];
                 
 
             }
             
+
+            // TODO: si header[0] (que indica que la antena está 
+            // en el pad debemos agregarla), de lo contrario no.
             listadoAntenas.push(antena);
         }
     }
@@ -32,7 +35,44 @@ function processData(allText) {
 
 }
 
-//ajax para obtener archvio CSV
+
+var itemsDebug;
+function getOperational(){
+
+    $.ajax({
+        type: "GET",
+        url: "data/operational.xml",
+        dataType: "xml",
+        success: function(xml) {
+          
+            // Obtenemos los items "Debug" del XML 
+            xmlDebug = $(xml).find("Debug");
+
+            // Filtramos aquellos que el id corresponda al de una antena
+            itemsDebug = $(xmlDebug).filter(function(){
+
+                // Si el string que identifica la antena está en el atributo "SourceObject"
+                if($(this).attr('SourceObject').indexOf("CM05") >= 0)
+                  return this;
+            });
+
+            //console.log(itemsDebug);
+
+            $.each(itemsDebug, function(i, item){
+              //console.log(item.);
+            });
+
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    });
+    return true;
+}
+
+
+// ajax para obtener archvio CSV
 function getPadInfo(){
     $.ajax({
         type: "POST",
@@ -40,18 +80,4 @@ function getPadInfo(){
         dataType: "text",
         success: function(data) {
             
-            processData(data);
-        }
-    });
-}
-
-function getOperational(){
-    $.ajax({
-        type: "GET",
-        url: "data/operational.xml",
-        dataType: "xml",
-        success: function(data) {
-            xml = data.find("Log");
-        }
-    });
-}
+      
