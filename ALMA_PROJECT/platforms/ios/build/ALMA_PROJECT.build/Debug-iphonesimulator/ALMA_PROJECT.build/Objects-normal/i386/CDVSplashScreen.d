11eeb61543870ddb9270e1ccd7730a56f5d45773
Andrés Contreras <aecontrerasl@gmail.com>
2014-04-13 10:14:46 -0300

    Muchos cambios no se

---
+++ ALMA_PROJECT/platforms/ios/build/ALMA_PROJECT.build/Debug-iphonesimulator/ALMA_PROJECT.build/Objects-normal/i386/CDVSplashScreen.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/ALMA_PROJECT/Plugins/org.apache.cordova.splashscreen/CDVSplashScreen.m \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/ALMA_PROJECT/Plugins/org.apache.cordova.splashscreen/CDVSplashScreen.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVPlugin.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVPluginResult.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/NSMutableArray+QueueAdditions.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVCommandDelegate.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVAvailability.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVInvokedUrlCommand.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVViewController.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVCommandQueue.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVWhitelist.h \
+  /Library/WebServer/Documents/spaceapps_araucateam/ALMA_PROJECT/platforms/ios/build/emulator/include/Cordova/CDVScreenOrientationDelegate.h
